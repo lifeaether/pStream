79f033d8552fab74ec237a665f9107a19ae3ed83
lifeaether <lifeaether@lifeaether-no-iMac.local>
2013-11-22 23:36:13 +0900

    Implement preferences

---
+++ PixivStream/PSPreferenceWindowController.h
@@ -0,0 +1,13 @@
+//
+//  PSPreferenceWindowController.h
+//  PixivStream
+//
+//  Created by lifeaether on 2013/11/22.
+//  Copyright (c) 2013年 lifeaether. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PSPreferenceWindowController : NSWindowController
+
+@end
---
+++ PixivStream/PSPreferenceWindowController.m
@@ -0,0 +1,22 @@
+//
+//  PSPreferenceWindowController.m
+//  PixivStream
+//
+//  Created by lifeaether on 2013/11/22.
+//  Copyright (c) 2013年 lifeaether. All rights reserved.
+//
+
+#import "PSPreferenceWindowController.h"
+
+@interface PSPreferenceWindowController ()
+
+@end
+
+@implementation PSPreferenceWindowController
+
+- (id)init
+{
+    return [super initWithWindowNibName:@"Preferences"];
+}
+
+@end
